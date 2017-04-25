ec7e294ca780e4180d6c608de85901791e53643e
Jonas Scherer <jonas@matilhaestud.io>
2017-04-25 12:08:20 -0300

    excluding www subdomain

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,6 @@ Bundler.require(*Rails.groups)
 
 module Multitenancy
   class Application < Rails::Application
-    # selects tennat base on subdomain
-    config.middleware.use Apartment::Elevators::Subdomain
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
